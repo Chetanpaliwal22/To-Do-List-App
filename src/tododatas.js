f351c6ff2571ac2ee3a642bcccd7b2c97b2e1386
Chetan Paliwal <Chetanpaliwal22@gmail.com>
2020-04-28 12:10:24 -0400

    completed the static design

---
+++ src/tododatas.js
@@ -0,0 +1,14 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Implement an to-do app using react",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "push the code to git and update readme",
+        completed: true
+    } 
+]
+
+export default todosData
