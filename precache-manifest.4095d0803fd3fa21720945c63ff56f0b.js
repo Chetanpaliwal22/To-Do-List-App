0ec75154b0617f4fe40f5c8ece25c60b2298b6ea
Chetan Paliwal <Chetanpaliwal22@gmail.com>
2020-05-02 19:09:30 -0400

    Updates

---
+++ precache-manifest.4095d0803fd3fa21720945c63ff56f0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0442ae25ce5c242812e7d76f1118d75",
+    "url": "/To-Do-List-App/index.html"
+  },
+  {
+    "revision": "af9af1a3d2b0ca2d3d17",
+    "url": "/To-Do-List-App/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "9dfdfffff5e64e9a7666",
+    "url": "/To-Do-List-App/static/css/main.ade30d98.chunk.css"
+  },
+  {
+    "revision": "af9af1a3d2b0ca2d3d17",
+    "url": "/To-Do-List-App/static/js/2.143e4a65.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/To-Do-List-App/static/js/2.143e4a65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dfdfffff5e64e9a7666",
+    "url": "/To-Do-List-App/static/js/main.67dd93d8.chunk.js"
+  },
+  {
+    "revision": "166c1a6130b4294196db",
+    "url": "/To-Do-List-App/static/js/runtime-main.565ba4d7.js"
+  }
+]);
