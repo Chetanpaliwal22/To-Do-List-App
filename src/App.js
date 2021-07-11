7a7cdecba6b638e03be48674b4f52df948b2e186
Chetanpaliwal22 <chetanpaliwal22@gmail.com>
2021-07-10 23:42:43 -0400

    minor refactoring

--- src/App.js
+++ src/App.js
@@ -1,139 +1,17 @@
 import React from 'react'
-import TodoItem from './ToDoItem'
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
-import { db } from './firebase'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import firebase from "firebase";
+import Main from './components/MainComponent';
 
 class App extends React.Component {
-  constructor() {
-    super()
-
-    this.state = {
-      todos: [],
-      showForm: false,
-      userInfo: {}
-    }
-    this.deleteTask = this.deleteTask.bind(this);
-  }
-
-  async componentDidMount() {
-    //Signing in anonymous user
-    try {
-      await firebase.auth().signInAnonymously()
-    }
-    catch (error) {
-      //Handle Error here
-    }
-
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        // User is signed in.
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        let userInfo = {
-          isAnonymous,
-          uid
-        }
-        this.setState({ userInfo })
-      } else {
-        // User is signed out.
-      }
-    });
-
-    this.updateContent()
-  }
-
-  updateContent = async () => {
-    let tododatas = await db.collection("ToDoList").get()
-    let list = tododatas.docs.map(doc => {
-      return doc.data()
-    })
-    this.setState({
-      todos: list
-    })
-  }
-
-  handleChange = (id) => {
-    let updatedTodos = this.state.todos.map(todo => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed
-      }
-      return todo
-    })
-    this.setState(updatedTodos)
-  }
 
   render() {
-    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />)
-
     return (
-      <div className="todo-list">
-        <div >
-          <h1> TO-DO LIST </h1>
-          <h3>Hey there, what's on your mind today?</h3>
-          {todoItems}
-        </div>
-        {this.state.showForm ? this.showForm() : null}
-        <div className="todo-button">
-          <Button variant="outline-success" onClick={() => this.setState({ showForm: true })}>Add Task</Button>
-          <Button variant="outline-danger" onClick={this.deleteTask}>Delete Task</Button>
-        </div>
+      <div className="App">
+        <Main/>
       </div>
     )
-  }
-
-  showForm = () => {
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <Form.Group controlId="formTask">
-          <Form.Label>Task Detail</Form.Label>
-          <Form.Control type="text" maxLength={30} placeholder="Enter task name." required ref={node => (this.inputNode = node)} />
-          <Form.Text className="text-muted">
-            We'll be pushing this task to DB.
-          </Form.Text>
-        </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" required label="Prove that, you are not a robot." />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Add
-        </Button>
-      </Form>
-    );
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.setState({ showForm: false })
-    let id = Date.now();
-    let newTask = {
-      id: id,
-      text: this.inputNode.value,
-      completed: false
-    }
-    db.collection('ToDoList').doc(id.toString()).set(newTask).then(() => {
-      this.state.todos.push(newTask)
-      this.setState({
-        todos: this.state.todos,
-        showForm: false
-      })
-    }).catch((error) => {
-      console.error("Error writing document: ", error);
-    });
-  }
-
-  deleteTask = async () => {
-    this.state.todos.map(todo => {
-      if (todo.completed) {
-        db.collection("ToDoList")
-          .doc(todo.id.toString())
-          .delete()
-      }
-      return todo
-    })
-  }
+  };
 
 }
+
 export default App;
