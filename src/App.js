82e735e410cd6850d5d539c13f8aaaac5fc0a612
Chetanpaliwal22 <chetanpaliwal22@gmail.com>
2021-07-13 22:16:58 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Main from './components/MainComponent';
+import LoginComponent from './components/LoginComponent';
+import MainComponent from './components/MainComponent';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './App.css';
 
 class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <Main/>
-      </div>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path='/' component={MainComponent} />
+            <Route path="/sign-in" component={LoginComponent} />
+          </Switch>
+        </div>
+      </Router>
     )
   };
-
 }
 
 export default App;
---
+++ src/components/LoginComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class LoginComponent extends Component {
+
+    render() {
+        return (
+            <form>
+                <h3>Sign In</h3>
+            </form >
+        );
+    }
+}
+
+export default LoginComponent;
